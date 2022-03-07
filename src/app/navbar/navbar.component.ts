f9b26e15a5ceefa6b7b12cd7db86b13d0bbfd065
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-07 09:23:23 -0800

    Create navbar.component.ts

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+  constructor() {}
+}
