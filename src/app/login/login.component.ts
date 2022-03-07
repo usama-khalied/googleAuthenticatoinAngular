36fa7ec51d2743d900b623a9689b57b1971def48
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-03-07 09:24:13 -0800

    Create login.component.ts

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+ import { Component, OnInit } from '@angular/core';
+import { GoogleLoginProvider, SocialAuthService, SocialUser } from 'angularx-social-login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  ngOnInit(): void {
+    
+  }
+
+}
