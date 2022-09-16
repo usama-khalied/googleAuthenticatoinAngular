05ef02fbb238b236b940bf6bcf0ef755125aec29
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2022-09-16 16:52:55 +0500

    #Google_Authentication_Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,54 @@
-import { Component } from '@angular/core';
-import { SocialUser, SocialAuthService, GoogleLoginProvider } from 'angularx-social-login';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app works!';
-  user : SocialUser;
-  constructor(private authService : SocialAuthService) { }
+export class AppComponent implements AfterViewInit {
+  newArr:any 
+ngOnInit(): void {
+  
+}
+  constructor() { }
 
-  ngOnInit(): void {
-    this.authService.authState.subscribe((user) => {
-      this.user = user
-    })
+  ngAfterViewInit(): void {
+    google.accounts.id.initialize({
+      client_id: "236025958894-l05tha7iovc0ool81upch4i6gi91npe8.apps.googleusercontent.com",
+      callback: (response: any) => this.handleGoogleSignIn(response)
+    });
+    google.accounts.id.renderButton(
+      document.getElementById("buttonDiv"),
+      { size: "large", type: "icon", shape: "pill" } 
+       // customization attributes
+    );
   }
-signinwithgoogle(){ //For Sign in
-  this.authService.signIn(GoogleLoginProvider.PROVIDER_ID)
-}
-signoutgoogle(){ // For sign out User 
-  this.authService.signOut();
-// 
-}
+
+  handleGoogleSignIn(response: any) {
+    
+ 
+
+    // This next is for decoding the idToken to an object if you want to see the details.
+    let base64Url = response.credential.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+
+      return  '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+              
+    }).join(''));
+    
+    console.log(JSON.parse(jsonPayload));
+
+    let obj = JSON.parse(jsonPayload)
+    // return 
+    console.log(obj.name);
+    console.log(obj.email);
+    console.log(obj.picture)
+
+  
+  
+  }
+
 }
